df750962c55e4e64ce4031ce390bf0d4ce17a65b
ANNIE17ball <annerillanabalon@liceovvh.cl>
2026-07-15 12:52:14 -0400

    gfsnjgx

--- python/flask/fundamentos/8.-Ranking_jugadores/server.py
+++ python/flask/fundamentos/8.-Ranking_jugadores/server.py
@@ -15,21 +15,33 @@ jugadores = [
 # Ruta para mostrar el ranking de jugadores
 @app.route("/ranking")
 def ranking():
-    return render_template("index.html")
-
+    return render_template(
+        "index.html",
+        jugadores=jugadores,
+        color=None
+    )
 # Ruta para mostrar un número limitado de jugadores
-@app.route("/ranking/<int: num>")
-def ranking(num):
-    if 1 <= num <= len(jugadores):
-        jugador = next((j for j in jugadores if j["id"] <= num), None)
-        return render_template("index.html", jugador = jugador)
+@app.route("/ranking/<int:cantidad>")
+def ranking_limitado(cantidad):
+    if 1 <= cantidad <= 5:
+        return render_template("index.html",
+                                jugadores=jugadores[:cantidad],
+                                color=None)
     else:
         return abort(404)
 
 # Ruta para personalizar el color del ranking
-@app.route("/ranking")
-def ranking():
-    return render_template("index.html")
+@app.route("/ranking/<int:cantidad>/<color>")
+def ranking_color(cantidad, color):
+    return render_template("index.html",
+                        jugadores=jugadores[:cantidad],
+                        color=color
+                        )
+
+# BONUS: Página de error personalizada si el usuario ingresa una ruta inexistente
+@app.errorhandler(404)
+def pagina_no_encontrada(error):
+    return "⚠️ Error 404 - Página no encontrada.", 404
 
 # Ejecutar el servidor
 if __name__ == "__main__":
