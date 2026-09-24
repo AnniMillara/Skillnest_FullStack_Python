6d1113c659a52f6d6df2c44a4dde84e03c37780e
ANNIE17ball <annerillanabalon@liceovvh.cl>
2026-09-24 09:08:47 -0300

    cambios

--- python/cores/Usuarios_CRUD_core_anabalon_annerill_20260923/flask_app/config/mysqlconnection.py
+++ python/cores/Usuarios_CRUD_core_anabalon_annerill_20260923/flask_app/config/mysqlconnection.py
@@ -6,7 +6,7 @@ class MySQLConnection:
         connection = pymysql.connect(
             host="localhost",
             user="root",
-            password="root",
+            password="1234",
             database=db,
             charset="utf8mb4",
             cursorclass=pymysql.cursors.DictCursor,
